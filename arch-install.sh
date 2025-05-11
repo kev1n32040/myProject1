6af8bbae3caf42ed3b8a5d49cb9fc535be83d9ee
creator bots <107846467+kev1n32040@users.noreply.github.com>
2025-05-11 20:19:02 +0300

    Update arch-install.sh

--- arch-install.sh
+++ arch-install.sh
@@ -25,3 +25,81 @@ fi
 echo "Installing in $boot_mode mode..."
 
 # Очистка и разметка диска
+umount -R /mnt || true
+wipefs -af "$disk"
+sgdisk -Zo "$disk"
+
+if [ "$boot_mode" = "UEFI" ]; then
+    # UEFI: EFI + root
+    sgdisk -n1:0:+300M -t1:ef00 "$disk"
+    sgdisk -n2:0:0     -t2:8300 "$disk"
+    part_boot="${disk}1"
+    part_root="${disk}2"
+else
+    # BIOS: BIOS boot + root
+    sgdisk -a1 -n1:2048:+1M -t1:ef02 "$disk"
+    sgdisk -n2:0:0     -t2:8300 "$disk"
+    part_boot=""
+    part_root="${disk}2"
+fi
+
+sleep 2
+
+# Форматирование
+if [ "$boot_mode" = "UEFI" ]; then
+    mkfs.fat -F32 "$part_boot"
+fi
+mkfs.ext4 -F "$part_root"
+
+# Монтирование
+mount "$part_root" /mnt
+if [ "$boot_mode" = "UEFI" ]; then
+    mkdir -p /mnt/boot
+    mount "$part_boot" /mnt/boot
+fi
+
+# Установка базовой системы
+pacstrap /mnt base linux linux-firmware bash sudo nano networkmanager grub efibootmgr
+
+# Генерация fstab
+genfstab -U /mnt >> /mnt/etc/fstab
+
+# Настройка в chroot
+arch-chroot /mnt /bin/bash <<EOF
+ln -sf /usr/share/zoneinfo/$timezone /etc/localtime
+hwclock --systohc
+
+echo "$locale" > /etc/locale.gen
+locale-gen
+echo "LANG=$locale_conf" > /etc/locale.conf
+
+echo "$hostname" > /etc/hostname
+cat <<HOSTS > /etc/hosts
+127.0.0.1   localhost
+::1         localhost
+127.0.1.1   $hostname.localdomain $hostname
+HOSTS
+
+# Создание пользователя
+useradd -m -G wheel -s /bin/bash $username
+echo "$username:$password" | chpasswd
+echo "root:$password" | chpasswd
+
+# Разрешаем sudo для wheel
+sed -i 's/^# %wheel ALL=(ALL:ALL) ALL/%wheel ALL=(ALL:ALL) ALL/' /etc/sudoers
+
+# Включаем сетевые службы
+systemctl enable NetworkManager
+
+# Установка GRUB
+if [ "$boot_mode" = "UEFI" ]; then
+    mkdir -p /boot/EFI
+    grub-install --target=x86_64-efi --efi-directory=/boot --bootloader-id=GRUB
+else
+    grub-install --target=i386-pc "$disk"
+fi
+
+grub-mkconfig -o /boot/grub/grub.cfg
+EOF
+
+echo -e "\n✅ Установка завершена! Можно перезагружать."
