dde81835b4b0baf878a3cf977eeadb9885199167
creator bots <107846467+kev1n32040@users.noreply.github.com>
2025-05-11 20:18:37 +0300

    Update arch-install.sh

--- arch-install.sh
+++ arch-install.sh
@@ -15,82 +15,13 @@ timezone="Asia/Krasnoyarsk"
 locale="en_US.UTF-8 UTF-8"
 locale_conf="en_US.UTF-8"
 
-# Check boot mode
+# Проверка загрузки: UEFI или BIOS
 if [ -d /sys/firmware/efi ]; then
     boot_mode="UEFI"
 else
     boot_mode="BIOS"
 fi
 
-echo "Installing in $boot_mode mode."
+echo "Installing in $boot_mode mode..."
 
-# Clear and partition disk
-wipefs -af "$disk"
-sgdisk -Zo "$disk"
-
-if [ "$boot_mode" = "UEFI" ]; then
-    # UEFI partitions
-    sgdisk -n1:0:+300M -t1:ef00 "$disk"
-    sgdisk -n2:0:0     -t2:8300 "$disk"
-    part_boot="${disk}1"
-    part_root="${disk}2"
-else
-    # BIOS partitions
-    sgdisk -a 1 -n1:2048:+1M -t1:ef02 "$disk"   # BIOS boot partition
-    sgdisk -n2:0:0     -t2:8300 "$disk"
-    part_root="${disk}2"
-fi
-
-sleep 1
-
-# Format partitions
-if [ "$boot_mode" = "UEFI" ]; then
-    mkfs.fat -F32 "$part_boot"
-fi
-mkfs.ext4 -F "$part_root"
-
-# Mount
-mount "$part_root" /mnt
-if [ "$boot_mode" = "UEFI" ]; then
-    mkdir /mnt/boot
-    mount "$part_boot" /mnt/boot
-fi
-
-# Install base system
-pacstrap /mnt base linux linux-firmware bash sudo nano networkmanager grub efibootmgr
-
-# Generate fstab
-genfstab -U /mnt >> /mnt/etc/fstab
-
-# Chroot and configure system
-arch-chroot /mnt /bin/bash <<EOF
-
-ln -sf /usr/share/zoneinfo/$timezone /etc/localtime
-hwclock --systohc
-
-echo "$locale" > /etc/locale.gen
-locale-gen
-
-echo "LANG=$locale_conf" > /etc/locale.conf
-echo "$hostname" > /etc/hostname
-echo -e "127.0.0.1\tlocalhost\n::1\tlocalhost\n127.0.1.1\t$hostname.localdomain\t$hostname" > /etc/hosts
-
-useradd -m -G wheel -s /bin/bash $username
-echo "$username:$password" | chpasswd
-echo "root:$password" | chpasswd
-
-sed -i 's/^# %wheel ALL=(ALL:ALL) ALL/%wheel ALL=(ALL:ALL) ALL/' /etc/sudoers
-
-systemctl enable NetworkManager
-
-if [ "$boot_mode" = "UEFI" ]; then
-    grub-install --target=x86_64-efi --efi-directory=/boot --bootloader-id=GRUB
-else
-    grub-install --target=i386-pc "$disk"
-fi
-
-grub-mkconfig -o /boot/grub/grub.cfg
-EOF
-
-# Done
-echo "\nInstallation complete! You can reboot now."
+# Очистка и разметка диска
