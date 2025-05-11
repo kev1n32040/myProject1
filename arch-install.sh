456199d4be5626d42d7e7b532a7dbdd10e32b713
creator bots <107846467+kev1n32040@users.noreply.github.com>
2025-05-11 19:54:11 +0300

    Update arch-install.sh

--- arch-install.sh
+++ arch-install.sh
@@ -1,70 +1,59 @@
 #!/bin/bash
 
-set -e
+# Устанавливаем зеркало поближе
+reflector --country Russia --latest 5 --sort rate --save /etc/pacman.d/mirrorlist
 
-DISK="/dev/sda"
-HOSTNAME="archhypr"
-USERNAME="user"
-PASSWORD="password"
-TIMEZONE="Asia/Krasnoyarsk"
+# Устанавливаем ntp
+timedatectl set-ntp true
 
-# Разметка и форматирование
-parted -s $DISK mklabel gpt
-parted -s $DISK mkpart ESP fat32 1MiB 512MiB
-parted -s $DISK set 1 esp on
-parted -s $DISK mkpart primary ext4 512MiB 100%
+# Разметка диска
+umount -R /mnt 2>/dev/null
+wipefs -a /dev/sda
+parted -s /dev/sda mklabel gpt
+parted -s /dev/sda mkpart primary ext4 1MiB 100%
+mkfs.ext4 /dev/sda1
+mount /dev/sda1 /mnt
 
-mkfs.fat -F32 ${DISK}1
-mkfs.ext4 ${DISK}2
+# Установка базовой системы
+pacstrap /mnt base linux linux-firmware sudo vim grub
 
-# Монтирование
-mount ${DISK}2 /mnt
-mkdir /mnt/boot
-mount ${DISK}1 /mnt/boot
-
-# Базовая система
-pacstrap -K /mnt base linux linux-firmware grub efibootmgr networkmanager sudo git neovim
-
-# fstab
+# Генерация fstab
 genfstab -U /mnt >> /mnt/etc/fstab
 
-# chroot
-arch-chroot /mnt /bin/bash <<EOF
+# Настройка внутри установленной системы
+arch-chroot /mnt /bin/bash -e <<EOF
 
-# Язык и часовой пояс
-ln -sf /usr/share/zoneinfo/$TIMEZONE /etc/localtime
+# Установка временной зоны и локали
+ln -sf /usr/share/zoneinfo/Europe/Moscow /etc/localtime
 hwclock --systohc
 echo "en_US.UTF-8 UTF-8" >> /etc/locale.gen
 locale-gen
 echo "LANG=en_US.UTF-8" > /etc/locale.conf
 
-# Хостнейм и сеть
-echo "$HOSTNAME" > /etc/hostname
-echo "127.0.0.1 localhost" >> /etc/hosts
-echo "::1       localhost" >> /etc/hosts
-echo "127.0.1.1 $HOSTNAME.localdomain $HOSTNAME" >> /etc/hosts
+# Сетевое имя
+echo arch-pc > /etc/hostname
 
-# Пользователь
-useradd -m -G wheel -s /bin/bash $USERNAME
-echo "$USERNAME:$PASSWORD" | chpasswd
-echo "root:$PASSWORD" | chpasswd
-echo "%wheel ALL=(ALL) ALL" >> /etc/sudoers
+# Hosts
+cat <<EOT > /etc/hosts
+127.0.0.1   localhost
+::1         localhost
+127.0.1.1   arch-pc.localdomain arch-pc
+EOT
 
-# Размонтировать все точки, если /mnt уже используется
-if mount | grep -q '/mnt'; then
-    echo "Размонтирование /mnt и всех подмонтированных точек..."
-    umount -R /mnt
-    sleep 2
-fi
+# Пароль root
+echo root:root | chpasswd
 
-# Загрузчик
-grub-install --target=x86_64-efi --efi-directory=/boot --bootloader-id=GRUB
+# Создание пользователя user с паролем 1234
+useradd -m -G wheel -s /bin/bash user
+echo user:1234 | chpasswd
+
+# Разрешение sudo
+sed -i 's/^# %wheel ALL=(ALL:ALL) ALL/%wheel ALL=(ALL:ALL) ALL/' /etc/sudoers
+
+# Установка загрузчика
+grub-install --target=i386-pc /dev/sda
 grub-mkconfig -o /boot/grub/grub.cfg
-
-# Сеть
-systemctl enable NetworkManager
-
 EOF
 
-umount -R /mnt
-echo "Установка завершена. Перезагрузитесь!"
+# Готово
+echo "Установка завершена. Перезагрузитесь после выхода."
