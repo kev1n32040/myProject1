0393ea8e02b17ce09992f4be1bb0fccb55622feb
creator bots <107846467+kev1n32040@users.noreply.github.com>
2025-05-12 05:57:33 +0300

    Update arch-install.sh

--- arch-install.sh
+++ arch-install.sh
@@ -2,7 +2,7 @@
 
 # Arch Linux Auto Install Script (BIOS + UEFI)
 # Timezone: Asia/Krasnoyarsk
-# Author: Keu1n (https://github.com/keu1n32040)
+# Author: kev1n32040
 
 set -euo pipefail
 
@@ -15,85 +15,73 @@ timezone="Asia/Krasnoyarsk"
 locale="en_US.UTF-8 UTF-8"
 locale_conf="en_US.UTF-8"
 
-# Проверка загрузки: UEFI или BIOS
+# Check boot mode
 if [ -d /sys/firmware/efi ]; then
     boot_mode="UEFI"
 else
     boot_mode="BIOS"
 fi
 
-echo "Installing in $boot_mode mode..."
+echo "Installing in $boot_mode mode."
 
-# Очистка и разметка диска
-umount -R /mnt || true
+# Clear and partition disk
 wipefs -af "$disk"
 sgdisk -Zo "$disk"
 
 if [ "$boot_mode" = "UEFI" ]; then
-    # UEFI: EFI + root
     sgdisk -n1:0:+300M -t1:ef00 "$disk"
     sgdisk -n2:0:0     -t2:8300 "$disk"
     part_boot="${disk}1"
     part_root="${disk}2"
 else
-    # BIOS: BIOS boot + root
-    sgdisk -a1 -n1:2048:+1M -t1:ef02 "$disk"
+    sgdisk -a 1 -n1:2048:+1M -t1:ef02 "$disk"
     sgdisk -n2:0:0     -t2:8300 "$disk"
-    part_boot=""
     part_root="${disk}2"
 fi
 
 sleep 2
 
-# Форматирование
+# Format partitions
 if [ "$boot_mode" = "UEFI" ]; then
     mkfs.fat -F32 "$part_boot"
 fi
 mkfs.ext4 -F "$part_root"
 
-# Монтирование
+# Mount
 mount "$part_root" /mnt
 if [ "$boot_mode" = "UEFI" ]; then
     mkdir -p /mnt/boot
     mount "$part_boot" /mnt/boot
 fi
 
-# Установка базовой системы
+# Install base system
 pacstrap /mnt base linux linux-firmware bash sudo nano networkmanager grub efibootmgr
 
-# Генерация fstab
+# Generate fstab
 genfstab -U /mnt >> /mnt/etc/fstab
 
-# Настройка в chroot
+# Chroot and configure system
 arch-chroot /mnt /bin/bash <<EOF
+
 ln -sf /usr/share/zoneinfo/$timezone /etc/localtime
 hwclock --systohc
 
 echo "$locale" > /etc/locale.gen
 locale-gen
+
 echo "LANG=$locale_conf" > /etc/locale.conf
-
 echo "$hostname" > /etc/hostname
-cat <<HOSTS > /etc/hosts
-127.0.0.1   localhost
-::1         localhost
-127.0.1.1   $hostname.localdomain $hostname
-HOSTS
+echo -e "127.0.0.1\tlocalhost\n::1\tlocalhost\n127.0.1.1\t$hostname.localdomain\t$hostname" > /etc/hosts
 
-# Создание пользователя
 useradd -m -G wheel -s /bin/bash $username
 echo "$username:$password" | chpasswd
 echo "root:$password" | chpasswd
 
-# Разрешаем sudo для wheel
 sed -i 's/^# %wheel ALL=(ALL:ALL) ALL/%wheel ALL=(ALL:ALL) ALL/' /etc/sudoers
 
-# Включаем сетевые службы
 systemctl enable NetworkManager
 
-# Установка GRUB
 if [ "$boot_mode" = "UEFI" ]; then
-    mkdir -p /boot/EFI
     grub-install --target=x86_64-efi --efi-directory=/boot --bootloader-id=GRUB
 else
     grub-install --target=i386-pc "$disk"
@@ -102,4 +90,6 @@ fi
 grub-mkconfig -o /boot/grub/grub.cfg
 EOF
 
-echo -e "\n✅ Установка завершена! Можно перезагружать."
+# Done
+echo -e "\nInstallation complete! You can reboot now."
+ завершена! Можно перезагружать."
